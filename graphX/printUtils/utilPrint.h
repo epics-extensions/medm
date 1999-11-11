6c28bd8dbfd95fa6a1f5b2c80dfb5e841508f592
Ken Evans <evans@aps.anl.gov>
1999-11-11 13:32:09 -0600

    Removed graphX/printUtils/*.

--- graphX/printUtils/utilPrint.h
+++
@@ -1,9 +0,0 @@
-/* Header file for print utilities */
-
-#ifndef __UTIL_PRINT_H__
-#define __UTIL_PRINT_H__
-
-/* Function prototypes */
-void utilPrint(Display *display, Window window, char *fileName);
-
-#endif  /* __UTIL_PRINT_H__ */
